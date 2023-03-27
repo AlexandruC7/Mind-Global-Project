ea1535affa1ae955c643d1619434ba3437d1ae26
Daniela Dragomiretcaia <daniela.dragomiretcaia@gmail.com>
2023-03-27 19:59:35 +0300

    Changes

---
+++ src/components/desprenoi/desprenoi.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./desprenoi.css";
+function desprenoi() {
+  return (
+    <div className="desprenoi">
+      <h1>DESPRE NOI</h1>
+      <p>
+        Suntem o companie tânără și ambițioasă. Spațiul media creaază noi
+        posibilități pe care noi nu le ratăm. Chiar dacă suntem noi pe piață,
+        prestăm o gamă larga de servicii și produse calitative . Mai jos puteți
+        vedea câte ceva despre identitatea noastră corporativă
+      </p>
+      <video src="/Green Gradient Business Digital Marketing Intro Youtube Video (1) (1) (1).mp4" />
+    </div>
+  );
+}
+
+export default desprenoi;
