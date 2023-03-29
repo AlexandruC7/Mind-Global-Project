abfface6c7a8b67837d5c57d80f764f79c390de4
Jeniacont <jeniabortnic@gmail.com>
2023-03-30 01:52:16 +0300

    add video for homepage

--- src/components/homepage/homepage.jsx
+++ src/components/homepage/homepage.jsx
@@ -1,7 +1,22 @@
 import "./homepage.css"
+import BgVideo from './video-bg.mp4'
+
 
 export const Homepage = () => {
   return (
-  <div className="homepage">homepage</div>
+  <div className="homepage">
+    <video src={BgVideo} autoPlay muted loop class="video-bg"/>
+
+    <div className="bg-overlay" ></div>
+
+    <div className="home-text">
+      <h1>Mind Global</h1>
+      <p> Design grafic - dezvoltare web - promovare tv / radio / web</p>
+      <div className="home-btn"> <a href="#">Aflați mai multe</a></div>
+    </div>
+
+   
+
+  </div>
   );
 }
