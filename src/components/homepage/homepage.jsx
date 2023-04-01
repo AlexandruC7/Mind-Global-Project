ad4426aca9d954cad3bb336293635896afc38a7b
Jeniacont <jeniabortnic@gmail.com>
2023-04-01 16:21:53 +0300

    fix bugs in homepage

--- src/components/homepage/homepage.jsx
+++ src/components/homepage/homepage.jsx
@@ -1,21 +1,16 @@
-import "./homepage.css";
+import "./homepage.css"
+
 
 export const Homepage = () => {
   return (
-    <div className="homepage">
-      <video src="./video-bg.mp4" autoPlay muted loop class="video-bg" />
-
-      <div className="bg-overlay"></div>
-
-      <div className="home-text">
-        <h1>Mind Global</h1>
-
-        <p> Design grafic - dezvoltare web - promovare tv / radio / web</p>
-        <div className="home-btn">
-          {" "}
-          <a href="#">Aflați mai multe</a>
-        </div>
-      </div>
+  <div className="homepage">
+    <video src="./video-bg.mp4" autoPlay muted loop class="video-bg"/>
+    <div className="bg-overlay" ></div>
+    <div className="home-text">
+      <h1 className="homepage__h1">Mind Global</h1>
+      <p className="homepage__p"> Design grafic - dezvoltare web - promovare tv / radio / web</p>
+      <div className="home-btn"> <a href="https://www.youtube.com/" className="homepage__a">Aflați mai multe</a></div>
     </div>
+  </div>
   );
-};
+}
