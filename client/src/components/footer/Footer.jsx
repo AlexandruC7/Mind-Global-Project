c5f8a1a35254c269af8de45c944eb7244ac0449b
Jeniacont <jeniabortnic@gmail.com>
2023-04-14 19:19:17 +0300

    add footer

---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,71 @@
+import "./footer.css";
+
+export const Footer = () => {
+    return (
+      <div className="footer__info">
+   
+        <footer class="footer-distributed">
+
+			<div class="footer-left">
+
+				<h3>Mind <span>Global</span></h3>
+
+				<p class="footer-links">
+					<a href="https://en.wikipedia.org/wiki/Wiki" class="link-1">Acasa</a>
+					
+					<a href="https://en.wikipedia.org/wiki/Wiki">Despre Noi</a>
+				
+					<a href="https://en.wikipedia.org/wiki/Wiki">Servicii</a>
+				
+					<a href="https://en.wikipedia.org/wiki/Wiki">Preturi</a>
+					
+					<a href="https://en.wikipedia.org/wiki/Wiki">Cos</a>
+					
+					<a href="https://en.wikipedia.org/wiki/Wiki">Cont</a>
+				</p>
+
+				<p class="footer-company-name">Mind Global © 2023</p>
+			</div>
+
+			<div class="footer-center">
+
+				<div>
+					<i class="fa fa-map-marker"></i>
+					<p><span>or.Chișinău</span> str. Ștefan cel Mare</p>
+				</div>
+
+				<div>
+					<i class="fa fa-phone"></i>
+					<p>(+373)-78-11-11-11</p>
+				</div>
+
+				<div>
+					<i class="fa fa-envelope"></i>
+					<p><a href="mailto:mindcfem@gmail.com">mindcfem@gmail.com</a></p>
+				</div>
+
+			</div>
+
+			<div class="footer-right">
+
+				<p class="footer-company-about">
+					<span>Site Development - ClickSchool students</span>
+					Designed by ClickSchool students
+				</p>
+
+				<div class="footer-icons">
+
+					<a href="https://en.wikipedia.org/wiki/Wiki"><i class="fa fa-facebook"></i></a>
+					<a href="https://en.wikipedia.org/wiki/Wiki"><i class="fa fa-twitter"></i></a>
+					<a href="https://en.wikipedia.org/wiki/Wiki"><i class="fa fa-linkedin"></i></a>
+					<a href="https://en.wikipedia.org/wiki/Wiki"><i class="fa fa-github"></i></a>
+
+				</div>
+
+			</div>
+
+		</footer>
+      </div>
+    );
+  };
+  
